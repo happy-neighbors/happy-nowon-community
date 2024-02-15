21c89cd68a14f22b853b754a24d2d3e7415566cb
han <01ideale@gmail.com>
2024-02-15 14:22:11 +0900

    first commit

---
+++ src/main/java/com/project/community/HappyNowonCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.project.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyNowonCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyNowonCommunityApplication.class, args);
+	}
+
+}
