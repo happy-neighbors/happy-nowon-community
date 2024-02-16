1261ba9a4f1412a2cf439c763f39982a5af35e96
hyeongcheol <jangdol012@naver.com>
2024-02-16 17:50:16 +0900

    khc front

---
+++ src/main/resources/static/js/summernote/my-summernote.js
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+$(document).ready(function () {
+	$('#summernote').summernote({
+		toolbar: [
+			['fontname', ['fontname']],
+			['fontsize', ['fontsize']],
+			['style', ['bold', 'italic', 'underline', 'strikethrough', 'clear']],
+			['color', ['forecolor', 'color']],
+			['table', ['table']],
+			['para', ['ul', 'ol', 'paragraph']],
+			['height', ['height']],
+			['insert', ['picture', 'link', 'video']],
+			['view', ['fullscreen', 'help']]
+		],
+		height: "500",
+		lang: "ko-KR",
+		fontNames: ['Arial', 'Arial Black', 'Comic Sans MS', 'Courier New', '맑은 고딕', '궁서', '굴림체', '굴림', '돋움체', '바탕체'],
+		fontSizes: ['8', '9', '10', '11', '12', '14', '16', '18', '20', '22', '24', '28', '30', '36', '50', '72'],
+		callbacks: {	//여기 부분이 이미지를 첨부하는 부분
+			onImageUpload: function (files) {
+				uploadSummernoteImageFile(files[0], this);
+			},
+			onPaste: function (e) {
+				var clipboardData = e.originalEvent.clipboardData;
+				if (clipboardData && clipboardData.items && clipboardData.items.length) {
+					var item = clipboardData.items[0];
+					if (item.kind === 'file' && item.type.indexOf('image/') !== -1) {
+						e.preventDefault();
+					}
+				}
+			}
+		}
+	});
+});
