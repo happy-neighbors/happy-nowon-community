309e17d3fd88048339e14f7a330c25ba46498344
young jin <scn07074@naver.com>
2024-02-19 17:02:37 +0900

    commit

--- src/main/java/com/project/community/domain/entity/NoticeEntity.java
+++ src/main/java/com/project/community/domain/entity/NoticeEntity.java
@@ -1,5 +1,10 @@
 package com.project.community.domain.entity;
 
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.ColumnDefault;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -7,18 +12,29 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Data
+@Getter
 @Table(name = "notice")
-public class NoticeEntity {
+public class NoticeEntity extends DateEntity{
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private Long no;
+	@Column(nullable = false)
+	private String title;
+	@Column(nullable = false)
+	private String content;
+	@Column(nullable = false)
+	private boolean type;
+	private LocalDateTime start;
+	private LocalDateTime end;
+	@Column(nullable = false)
+	@ColumnDefault("0")
+	private boolean status;
 }
