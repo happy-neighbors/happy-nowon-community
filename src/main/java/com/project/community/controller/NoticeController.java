36ef293817b7ad07580ff95a6bc742d1f24928e1
young jin <scn07074@naver.com>
2024-02-22 18:30:44 +0900

    0222

--- src/main/java/com/project/community/controller/NoticeController.java
+++ src/main/java/com/project/community/controller/NoticeController.java
@@ -1,6 +1,7 @@
 package com.project.community.controller;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.security.core.Authentication;
 
 import com.project.community.domain.dto.NoticeDTO;
+import com.project.community.domain.dto.PageRequestDTO;
 import com.project.community.domain.entity.NoticeEntityRepository;
 import com.project.community.service.NoticeService;
 
@@ -24,48 +26,69 @@ public class NoticeController {
 	NoticeService nService;
 	@Autowired
 	NoticeEntityRepository noticeRepo;
-	
+
 	@GetMapping("/notice")
-	public String notice(Model model) {
+	public String notice(Model model, PageRequestDTO prDTO) {
+		model.addAttribute("page", nService.getlist(prDTO));
 		nService.noticeList(model);
 		return "notice/notice";
 	}
+
+	@GetMapping("/notice/search")
+	public String search(@RequestParam(value="keyword")String keyword, Model model) {
+		List<NoticeDTO> nDTO = nService.search(keyword);
+		model.addAttribute("keyList", nDTO);
+		return "notice/notice-search";
+	}
 	
+	@GetMapping("/notice-event")
+	public String noticeEvent(Model model, PageRequestDTO prDTO) {
+		model.addAttribute("page", nService.getlist(prDTO));
+		nService.noticeList(model);
+		return "notice/notice-event";
+	}
+
+	@GetMapping("/notice-notice")
+	public String noticeNotice(Model model, PageRequestDTO prDTO) {
+		model.addAttribute("page", nService.getlist(prDTO));
+		nService.noticeList(model);
+		return "notice/notice-notice";
+	}
+
 	@GetMapping("/add-notice")
 	public String noticeAdd() {
 		return "notice/add-notice";
 	}
-	
+
 	@PostMapping("/add-notice")
 	public String noticeAdd(NoticeDTO noticeDTO, Authentication user) {
 		nService.save(noticeDTO, user);
 		return "redirect:/notice";
 	}
-	
+
 	@GetMapping("/notice-detail/{no}")
-	public String noticeDetail(@PathVariable("no")long no, Model model) {
+	public String noticeDetail(@PathVariable("no") long no, Model model) {
 		nService.noticeDetail(no, model);
+		nService.getNotice(no);
 		return "notice/notice-detail";
 	}
-	
+
 	@GetMapping("/notice-edit/{no}")
-	public String noticeEditPage(@PathVariable("no")long no, Model model) {
+	public String noticeEditPage(@PathVariable("no") long no, Model model) {
 		nService.noticeDetail(no, model);
 		return "notice/notice-edit";
 	}
-	
+
 	@PutMapping("/notice-detail/{no}")
-	public String noticeEdit(@PathVariable("no")long no, 
-			@RequestParam("title")String title, 
-			@RequestParam("content")String content, 
-			@RequestParam("type")boolean type, 
-			@RequestParam("end")LocalDateTime end) {
+	public String noticeEdit(@PathVariable("no") long no, @RequestParam("title") String title,
+			@RequestParam("content") String content, @RequestParam("type") boolean type,
+			@RequestParam("end") LocalDateTime end) {
 		nService.noticeEdit(no, title, content, type, end);
 		return "redirect:/notice";
 	}
-	
+
 	@DeleteMapping("notice-detail/{no}")
-	public String noticeDelete(@PathVariable("no")long no) {
+	public String noticeDelete(@PathVariable("no") long no) {
 		nService.noticeDelete(no);
 		return "redirect:/notice";
 	}
