b21ae091ef4a4a6d9406c696fe342f4010b1f5fc
han <01ideale@gmail.com>
2024-02-27 17:58:46 +0900

    chatbot semifinal

--- src/main/java/com/project/community/movie/MovieService.java
+++ src/main/java/com/project/community/movie/MovieService.java
@@ -6,25 +6,24 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
-import org.springframework.ui.Model;
 
 @Service
 public class MovieService {
 	
 	private final String MovieUrl = "http://www.cgv.co.kr/movies/?lt=1&ft=1";
 	
-	public void getThumbnailUrl(Model model) {
+	public String getThumbnailUrl(String movieName) {
 		try {
 			Document document = Jsoup.connect(MovieUrl).get();
 			
-			Elements thumbnail = document.select("span.thumb-image > img[alt=건국전쟁 포스터]");
+			Elements thumbnail = document.select("img[alt=" + movieName + " 포스터]");
 			String thumbnailUrl = thumbnail.attr("src");
 			
-			System.out.println("thumbnail: " + thumbnailUrl);
-			
-			model.addAttribute("thumbnailUrl", thumbnailUrl);
+			return thumbnailUrl;
 		} catch (IOException e) {
 			e.printStackTrace();
+			
+			return null;
 		}
 	}
 }
